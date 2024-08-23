fa53baea7588219482acbb59fe41442f83b3f0e9
AmlanJSarmah <amlan.j.sarmah@gmail.com>
2024-08-23 14:06:12 +0530

    Added function to  move snake body

--- headers/utils.h
+++ headers/utils.h
@@ -7,5 +7,6 @@ WINDOW *draw_window();
 WINDOW *handle_window_resize();
 void handle_head_movement(WINDOW *win, int c, struct Snake *snake);
 void print_snake(WINDOW *win, struct Snake *snake);
+void move_snake_body(struct Snake *snake, int temp_x, int temp_y);
 
 #endif
--- utils.c
+++ utils.c
@@ -28,24 +28,156 @@ void handle_head_movement(WINDOW *win, int c, struct Snake *snake)
 
     switch (c)
     {
-    case ('w'):
-        if (snake->pos_0[1] > 1)
-            snake->pos_0[1] = snake->pos_0[1] - 1;
-        break;
-    case ('s'):
-        if (snake->pos_0[1] < (max_y - 2))
-            snake->pos_0[1] = snake->pos_0[1] + 1;
-        break;
-    case ('a'):
-        if (snake->pos_0[0] > 1)
-            snake->pos_0[0] = snake->pos_0[0] - 1;
-        break;
-    case ('d'):
-        if (snake->pos_0[0] < (max_x - 2))
-            snake->pos_0[0] = snake->pos_0[0] + 1;
-        break;
-    default:
-        break;
+        case ('w'):
+            if (snake->pos_0[1] > 1)
+            {
+                int temp_x = snake->pos_0[0];
+                int temp_y = snake->pos_0[1];
+                snake->pos_0[1] = snake->pos_0[1] - 1;
+                move_snake_body(snake, temp_x, temp_y);
+            }
+            break;
+        case ('s'):
+            if (snake->pos_0[1] < (max_y - 2))
+            {
+                int temp_x = snake->pos_0[0];
+                int temp_y = snake->pos_0[1];
+                snake->pos_0[1] = snake->pos_0[1] + 1;
+                move_snake_body(snake, temp_x, temp_y);
+            }
+            break;
+        case ('a'):
+            if (snake->pos_0[0] > 1)
+            {
+                int temp_x = snake->pos_0[0];
+                int temp_y = snake->pos_0[1];
+                snake->pos_0[0] = snake->pos_0[0] - 1;
+                move_snake_body(snake, temp_x, temp_y);
+            }
+            break;
+        case ('d'):
+            if (snake->pos_0[0] < (max_x - 2))
+            {
+                int temp_x = snake->pos_0[0];
+                int temp_y = snake->pos_0[1];
+                snake->pos_0[0] = snake->pos_0[0] + 1;
+                move_snake_body(snake, temp_x, temp_y);
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+void move_snake_body(struct Snake *snake, int temp_x, int temp_y)
+{
+    int _temp_x, _temp_y;
+    for (int i = 1; i < SNAKE_LENGTH; i++)
+    {
+        switch (i) 
+        {
+            case 1:
+                if (snake->pos_1[0] < 0)
+                    break;
+                _temp_x = snake->pos_1[0];
+                _temp_y = snake->pos_1[1];
+                snake->pos_1[0] = temp_x;
+                snake->pos_1[1] = temp_y;
+                temp_x = _temp_x;
+                temp_y = _temp_y;
+                break;
+
+            case 2:
+                if (snake->pos_2[0] < 0)
+                    break;
+                _temp_x = snake->pos_2[0];
+                _temp_y = snake->pos_2[1];
+                snake->pos_2[0] = temp_x;
+                snake->pos_2[1] = temp_y;
+                temp_x = _temp_x;
+                temp_y = _temp_y;
+                break;
+
+            case 3:
+                if (snake->pos_3[0] < 0)
+                    break;
+                _temp_x = snake->pos_3[0];
+                _temp_y = snake->pos_3[1];
+                snake->pos_3[0] = temp_x;
+                snake->pos_3[1] = temp_y;
+                temp_x = _temp_x;
+                temp_y = _temp_y;
+                break;
+
+            case 4:
+                if (snake->pos_4[0] < 0)
+                    break;
+                _temp_x = snake->pos_4[0];
+                _temp_y = snake->pos_4[1];
+                snake->pos_4[0] = temp_x;
+                snake->pos_4[1] = temp_y;
+                temp_x = _temp_x;
+                temp_y = _temp_y;
+                break;
+
+            case 5:
+                if (snake->pos_5[0] < 0)
+                    break;
+                _temp_x = snake->pos_5[0];
+                _temp_y = snake->pos_5[1];
+                snake->pos_5[0] = temp_x;
+                snake->pos_5[1] = temp_y;
+                temp_x = _temp_x;
+                temp_y = _temp_y;
+                break;
+
+            case 6:
+                if (snake->pos_6[0] < 0)
+                    break;
+                _temp_x = snake->pos_6[0];
+                _temp_y = snake->pos_6[1];
+                snake->pos_6[0] = temp_x;
+                snake->pos_6[1] = temp_y;
+                temp_x = _temp_x;
+                temp_y = _temp_y;
+                break;
+
+            case 7:
+                if (snake->pos_7[0] < 0)
+                    break;
+                _temp_x = snake->pos_7[0];
+                _temp_y = snake->pos_7[1];
+                snake->pos_7[0] = temp_x;
+                snake->pos_7[1] = temp_y;
+                temp_x = _temp_x;
+                temp_y = _temp_y;
+                break;
+
+            case 8:
+                if (snake->pos_8[0] < 0)
+                    break;
+                _temp_x = snake->pos_8[0];
+                _temp_y = snake->pos_8[1];
+                snake->pos_8[0] = temp_x;
+                snake->pos_8[1] = temp_y;
+                temp_x = _temp_x;
+                temp_y = _temp_y;
+                break;
+
+            case 9:
+                if (snake->pos_9[0] < 0)
+                    break;
+                _temp_x = snake->pos_9[0];
+                _temp_y = snake->pos_9[1];
+                snake->pos_9[0] = temp_x;
+                snake->pos_9[1] = temp_y;
+                temp_x = _temp_x;
+                temp_y = _temp_y;
+                break;
+
+            default:
+                break;
+        }
     }
 }
 
@@ -54,52 +186,53 @@ void print_snake(WINDOW *win, struct Snake *snake)
     // We use a reverse for loop to keep our cursor at the snake head
     // We also check if the snake postiton is valid or not.
     // -1 means the position isn't used yet. A positve value is a valid position
-    for (int i = SNAKE_LENGTH - 1; i > -1; i--)
+    for (int i = 0; i < SNAKE_LENGTH; i++)
     {
         switch (i)
         {
-        case 0:
-            if (snake->pos_0[0] > -1)
-                mvwprintw(win, snake->pos_0[1], snake->pos_0[0], "@");
-            break;
-        case 1:
-            if (snake->pos_1[0] > -1)
-                mvwprintw(win, snake->pos_1[1], snake->pos_1[0], "@");
-            break;
-        case 2:
-            if (snake->pos_2[0] > -1)
-                mvwprintw(win, snake->pos_2[1], snake->pos_2[0], "@");
-            break;
-        case 3:
-            if (snake->pos_3[0] > -1)
-                mvwprintw(win, snake->pos_3[1], snake->pos_3[0], "@");
-            break;
-        case 4:
-            if (snake->pos_4[0] > -1)
-                mvwprintw(win, snake->pos_4[1], snake->pos_4[0], "@");
-            break;
-        case 5:
-            if (snake->pos_5[0] > -1)
-                mvwprintw(win, snake->pos_5[1], snake->pos_5[0], "@");
-            break;
-        case 6:
-            if (snake->pos_6[0] > -1)
-                mvwprintw(win, snake->pos_6[1], snake->pos_6[0], "@");
-            break;
-        case 7:
-            if (snake->pos_7[0] > -1)
-                mvwprintw(win, snake->pos_7[1], snake->pos_7[0], "@");
-            break;
-        case 8:
-            if (snake->pos_8[0] > -1)
-                mvwprintw(win, snake->pos_8[1], snake->pos_8[0], "@");
-            break;
-        case 9:
-            if (snake->pos_9[0] > -1)
-                mvwprintw(win, snake->pos_9[1], snake->pos_9[0], "@");
-            break;
-        default:
-            break;
+            case 0:
+                if (snake->pos_0[0] > -1)
+                    mvwprintw(win, snake->pos_0[1], snake->pos_0[0], "@");
+                break;
+            case 1:
+                if (snake->pos_1[0] > -1)
+                    mvwprintw(win, snake->pos_1[1], snake->pos_1[0], "@");
+                break;
+            case 2:
+                if (snake->pos_2[0] > -1)
+                    mvwprintw(win, snake->pos_2[1], snake->pos_2[0], "@");
+                break;
+            case 3:
+                if (snake->pos_3[0] > -1)
+                    mvwprintw(win, snake->pos_3[1], snake->pos_3[0], "@");
+                break;
+            case 4:
+                if (snake->pos_4[0] > -1)
+                    mvwprintw(win, snake->pos_4[1], snake->pos_4[0], "@");
+                break;
+            case 5:
+                if (snake->pos_5[0] > -1)
+                    mvwprintw(win, snake->pos_5[1], snake->pos_5[0], "@");
+                break;
+            case 6:
+                if (snake->pos_6[0] > -1)
+                    mvwprintw(win, snake->pos_6[1], snake->pos_6[0], "@");
+                break;
+            case 7:
+                if (snake->pos_7[0] > -1)
+                    mvwprintw(win, snake->pos_7[1], snake->pos_7[0], "@");
+                break;
+            case 8:
+                if (snake->pos_8[0] > -1)
+                    mvwprintw(win, snake->pos_8[1], snake->pos_8[0], "@");
+                break;
+            case 9:
+                if (snake->pos_9[0] > -1)
+                    mvwprintw(win, snake->pos_9[1], snake->pos_9[0], "@");
+                break;
+            default:
+                break;
         }
     }
+    wmove(win, snake->pos_0[1], snake->pos_0[0]);
 }
